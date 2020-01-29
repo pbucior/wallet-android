43dfff0f9e53a3f56b19f93a6eaed4b51692393e
Paweł Bucior <pawel@bucior.eu>
2020-01-29 15:00:38 +0100

    Create Util class

---
+++ app/src/main/java/eu/bucior/wallet/util/Util.java
@@ -0,0 +1,15 @@
+package eu.bucior.wallet.util;
+
+public class Util {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "wallet";
+    public static final String TABLE_NAME = "operations";
+
+    public static final String KEY_ID = "id";
+    public static final String KEY_DATE_ADDED = "date_added";
+    public static final String KEY_DATE_OPERATION = "date_operation";
+    public static final String KEY_AMOUNT = "amount";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_POSTING_KEY = "posting_key";
+}
