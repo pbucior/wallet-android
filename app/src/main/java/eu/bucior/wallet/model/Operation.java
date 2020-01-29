2e1fe1a6618d92428ea3002a48bd14a3e6aa7540
Paweł Bucior <pawel@bucior.eu>
2020-01-29 14:54:45 +0100

    Create Operation class

---
+++ app/src/main/java/eu/bucior/wallet/model/Operation.java
@@ -0,0 +1,79 @@
+package eu.bucior.wallet.model;
+
+public class Operation {
+    private int id;
+    private int dateAdded;
+    private String dateOperation;
+    private double amount;
+    private String description;
+    private String postingKey;
+
+    public Operation(int dateAdded, String dateOperation, double amount, String description, String postingKey) {
+        this.dateAdded = dateAdded;
+        this.dateOperation = dateOperation;
+        this.amount = amount;
+        this.description = description;
+        this.postingKey = postingKey;
+    }
+
+    public Operation() {
+
+    }
+
+    public Operation(int id, int dateAdded, String dateOperation, double amount, String description, String postingKey) {
+        this.id = id;
+        this.dateAdded = dateAdded;
+        this.dateOperation = dateOperation;
+        this.amount = amount;
+        this.description = description;
+        this.postingKey = postingKey;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(int dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public String getDateOperation() {
+        return dateOperation;
+    }
+
+    public void setDateOperation(String dateOperation) {
+        this.dateOperation = dateOperation;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPostingKey() {
+        return postingKey;
+    }
+
+    public void setPostingKey(String postingKey) {
+        this.postingKey = postingKey;
+    }
+}
